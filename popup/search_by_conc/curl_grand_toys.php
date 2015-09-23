238e4a9e6f435bb546038033f5b16a000058b361
multitoys <multitoys.dp@gmail.com>
2015-09-23 17:40:54 +0300

    исправления для Грандтойс

--- popup/search_by_conc/curl_grand_toys.php
+++ popup/search_by_conc/curl_grand_toys.php
@@ -16,6 +16,7 @@
     include_once(DIR_CFG.'/connect.inc.wa.php');
     include(DIR_FUNC.'/setting_functions.php');
     include_once(DIR_COMPETITORS.'/curl_competitors.php');
+    include(DIR_COMPETITORS.'/grandtoys_categories.php');
 
     $DB_tree = new DataBase();
     $DB_tree->connect(SystemSettings::get('DB_HOST'), SystemSettings::get('DB_USER'), SystemSettings::get('DB_PASS'));
@@ -34,6 +35,7 @@
 
 TAG
     );
+    $usd = GetValue('currency_value', 'Conc__currency', 'CCID = 5');
 
     define('SLASH', '|');
     define('NAME_PATTERN', '<div\s+class="block[0-9]*?"[^<>]*?>[^<>]*?<div\s+class="product-overview-image"[^<>]*?>[^<>]*?<div\s+id="img-radius"[^<>]*?>[^<>]*?<a[^<>]*?>[^<>]*?</a>[^<>]*?</div>[^<>]*?</div>[^<>]*?<div\s+class="product-title"[^<>]*?>[^<>]*?<a[^<>]*?>[\s]*([^<>]+?)[\s]*</a>[^<>]*?</div>[^<>]*?');
@@ -43,65 +45,6 @@ TAG
     $headers = array
     (
         ''
-    );
-    $categories = array(
-        'Детские игрушки (4516)'      => array(
-            'Конструкторы (489)'          => '/ru/566-Konstruktori',
-            'Летний ассортимент (46)'     => '/ru/569-Letnii-assortiment',
-            'Мягкие игрушки (85)'         => '/ru/549-Myagkie-igrushki',
-            'Настольные игры (438)'       => '/ru/735-Nastolnie-igri',
-            'Развивающие игры (263)'      => '/ru/568-Razvivayuschie-igri',
-            'Творчество (53)'             => '/ru/1060-05Detskoe-tvorchestvo',
-            'Товары для девочек (717)'    => '/ru/570-Tovari-dlya-devochek',
-            'Товары для детей (728)'      => '/ru/571-Tovari-dlya-detei',
-            'Товары для мальчиков (1284)' => '/ru/572-Tovari-dlya-malchikov',
-            'Украинский пластик (412)'    => '/ru/577-Ukr-plastik'
-        ),
-        'Велосипеды (18)'             => array(
-            '2-х колесные (9)'  => '/ru/979-2-h-kolesnie',
-            '3-х колесные (5)'  => '/ru/977-3-h-kolesnie',
-            'Электромобили (4)' => '/ru/1142-Elektromobili'
-        ),
-        'Спорттовары (82)'            => array(
-            'Аксессуары для спорта (9)'   => '/ru/1074-Aksessuari-dlya-sporta',
-            'Бадминтоны (4)'              => '/ru/734-Badmintoni',
-            'Баскетбол (2)'               => '/ru/796-Basketbol',
-            'Боксерские наборы (1)'       => '/ru/535-Bokserskie-nabori',
-            'Дартс (4)'                   => '/ru/541-Darts',
-            'Защита, шлемы (1)'           => '/ru/249-Zaschita-shlemi',
-            'Мячи (7)'                    => '/ru/544-Myachi',
-            'Мячи резиновые детские (13)' => '/ru/396-Myachi-rezinovie-detskie',
-            'Ролики (11)'                 => '/ru/536-Roliki',
-            'Самокаты (15)'               => '/ru/538-Samokati',
-            'Скакалки (2)'                => '/ru/43-Skakalki',
-            'Скейты (5)'                  => '/ru/344-Skeiti',
-            'Теннис (5)'                  => '/ru/543-Tennis-',
-            'Хулахупы и обручи (3)'       => '/ru/61-Hulahupi-i-obruchi'
-        ),
-        'Надувные изделия (129)'      => array(
-            'BK Toys Ltd. (10)' => '/ru/1110-BK-Toys-Ltd',
-            'INTEX (119)'       => '/ru/1059-04INTEX'
-        ),
-        'Офисная канцелярия (1)'      => array(
-            'Блоки для заметок (1)' => '/ru/1030-Bloki-dlya-zametok'
-        ),
-        'Школьные принадлежности (7)' => array(
-            'Детское творчество (1)'       => '/ru/442-Detskoe-tvorchestvo',
-            'Закладки (1)'                 => '/ru/217-Zakladki',
-            'Рюкзаки и сумки (4)'          => '/ru/435-Ryukzaki',
-            'Чертежные принадлежности (1)' => '/ru/439-Chertezhnie-prinadlezhnosti'
-        ),
-        'Печатная продукция (25)'     => array(
-            'Книги (25)' => '/ru/426-Knigi'
-        ),
-        'Товары для детей (6)'        => array(
-            'Мебель для детей (6)' => '/ru/1016-11Mebel-dlya-detei'
-        ),
-        'Сувенирная продукция (6)'    => array(
-            'Наборы (1)'            => '/ru/1139-Podarochnie-paketi',
-            'Небесные фонарики (3)' => '/ru/1136-Nebesnie-fonariki',
-            'Открытки (2)'          => '/ru/1078-Otkritki'
-        )
     );
 
     define('URL_COMPETITORS', 'http://gtoys.com.ua');
@@ -157,8 +100,8 @@ TAG
                 $name = mysql_real_escape_string(trim(str_replace($replace_name, '', DecodeCodepage($products[1][$j]))));
                 $price = (double)$products[2][$j];
                 $code = mysql_real_escape_string(DecodeCodepage($products[3][$j]));
-                $price_usd = $price / 20.51;
                 $productID = GetValue('productID', 'Conc__grandtoys', "code = '$code'");
+                $price_usd = $price / $usd;
 
                 if ($productID) {
                     $query
---
+++ popup/search_by_conc/grandtoys_categories.php
@@ -0,0 +1,201 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Gololobov
+     * Date: 23.09.2015
+     * Time: 14:09
+     */
+
+    $categories
+        = array(
+        'Конструкторы'            => array(
+            'Конструктор JDLT'           => '/ru/283-Konstruktor-JDLT',
+            'Конструктор Kazi'           => '/ru/282-Konstruktor-Kazi',
+            'Конструктор Star'           => '/ru/277-Konstruktor-Star',
+            'Конструктор Super Hero'     => '/ru/546-Konstruktor-Super-Hero',
+            'Конструктор Xipco'          => '/ru/1023-Konstruktor-Xipco',
+            'Конструктор Город мастеров' => '/ru/1122-Konstruktor-Gorod-masterov',
+            'Конструкторы AUSINI, LIGAO' => '/ru/401-Konstruktori-AUSINI-LIGAO',
+            'Конструкторы BELA'          => '/ru/415-Konstruktori-BELA',
+            'Конструкторы Bela Friends'  => '/ru/1116-Konstruktori-Bela-Friends',
+            'Конструкторы BRICK'         => '/ru/405-Konstruktori-BRICK',
+            'Конструкторы CHIMA'         => '/ru/403-Konstruktori-CHIMA',
+            'Конструкторы COGO'          => '/ru/375-Konstruktori-COGO',
+            'Конструкторы Hero Factory'  => '/ru/1118-Konstruktori-Hero-Factory',
+            'Конструкторы JLB'           => '/ru/1108-JLB',
+            'Конструкторы Jubilux'       => '/ru/1121-Konstruktori-Jubilux',
+            'Конструкторы LELE'          => '/ru/1117-Konstruktori-LELE',
+            'Конструкторы Minecraft'     => '/ru/1119-Konstruktori-Minecraft',
+            'Конструкторы NINJA'         => '/ru/373-Konstruktori-NINJA',
+            'Конструкторы Play Smart'    => '/ru/1131-Konstruktori-Play-Smart',
+            'Конструкторы QLT'           => '/ru/1120-Konstruktori-QLT',
+            'Конструкторы Sluban'        => '/ru/371-Konstruktori-Sluban',
+            'Конструкторы Tondge'        => '/ru/1130-Kukla-Sofia',
+            'Конструкторы для малышей'   => '/ru/374-Konstruktori-dlya-malishei',
+            'Конструкторы МЕГАПОЛИС'     => '/ru/1127-Konstruktori-MEGAPOLIS',
+            'Конструкторы разные'        => '/ru/816-Konstruktori-raznie'),
+
+        'Летний ассортимент'      => array(
+            'Ветрячки, воздушные змеи,вертушки,сачки,запускалки' => '/ru/60-Vetryachki-vozdushnie-zmeivertushkisachkizapuskalki',
+            'Детские зонты, очки'                                => '/ru/37-Detskie-zonti-ochki',
+            'Песочные игры'                                      => '/ru/408-Pesochnie-igri'),
+
+        'Мягкие игрушки'          => array(
+            'Брелки, магниты, мячи, копилки' => '/ru/162-Brelki-magniti-myachi',
+            'Музыкальные'                    => '/ru/1135-Muzikalnie',
+            'Размер см более 50'             => '/ru/158-Razmer-sm-bolee-50',
+            'Размер см от 19 до 30'          => '/ru/156-Razmer-sm-ot-19-do-30',
+            'Размер см от 30 до 50'          => '/ru/157-Razmer-sm-ot-30-do-50',
+            'Сердца, подушки,маски'          => '/ru/161-Serdca-podushki'),
+
+        'Настольные игры'         => array(
+            'Домино и шахматы'      => '/ru/383-Domino-i-shahmati',
+            'Игры на магнитах'      => '/ru/798-Igri--na-magnitah',
+            'Игры с наклейками'     => '/ru/794-Igri--s-nakleikami',
+            'Логические'            => '/ru/739-Logicheskie',
+            'ЛотоДомино'            => '/ru/740-LotoDomino',
+            'Наборы для творчества' => '/ru/8-Nabori-dlya-tvorchestva',
+            'Пазлы'                 => '/ru/736-Pazli',
+            'Развивающие'           => '/ru/741-Razvivayuschie',
+            'Развлекательные'       => '/ru/742-Razvlekatelnie',
+            'Шашки,шахматы,нарды'   => '/ru/46-Shashki-i-shahmati',
+            'Экономические'         => '/ru/737-Ekonomicheskie',
+            'Эксперименты и опыты'  => '/ru/59-Eksperimenti-i-opiti'),
+
+        'Развивающие игры'        => array(
+            'Головоломки'                    => '/ru/350-Golovolomki',
+            'Деревянные развивающие игрушки' => '/ru/351-Derevyannie-razvivayuschie-igrushki',
+            'Доски для рисования,магнитные'  => '/ru/392-Doski-drisovaniya',
+            'Компьютеры, ноутбуки, планшеты' => '/ru/356-Kompyutera-noutbuki-plansheti',
+            'Кубики'                         => '/ru/419-Kubiki',
+            'Мозаики'                        => '/ru/406-Mozaiki',
+            'Мягкие кубики'                  => '/ru/926-Myagkie-kubiki',
+            'Обучающие игры'                 => '/ru/358-Obuchayuschie-igri',
+            'Оптические приборы'             => '/ru/762-Opticheskie-pribori',
+            'Сборные модели'                 => '/ru/345-Sbornie-modeli',
+            'Творчество'                     => '/ru/409-Tvorchestvo',
+            'Фомовые развивающие игрушки'    => '/ru/393-Fomovie-razvivayuschie-igrushki'),
+
+        'Творчество'              => array(
+            'Зірка'         => '/ru/980-5Zirka',
+            'Ранок'         => '/ru/945-3Ranok',
+            'Ранок Креатив' => '/ru/931-3Ranok',
+            'Роса'          => '/ru/1001-6Rosa'),
+
+        'Товары для девочек'      => array(
+            'Бижутерия, бисер, украшения, косметика,веера' => '/ru/397-Bizhuteriya-biser-ukrasheniya-kosmetika',
+            'Герои'                                        => '/ru/548-Geroi',
+            'Дома, мебель для Барби'                       => '/ru/352-Doma-mebel-dBarbi',
+            'Кареты, пони, лошади'                         => '/ru/378-Kareti-poni-loshadi',
+            'Коляски,кроватки'                             => '/ru/40-Kolyaski',
+            'Кукла Ever After High'                        => '/ru/1072-Ever-After-High',
+            'Кукла Monster High'                           => '/ru/394-Kukla-Monster-High',
+            'Кукла Winx'                                   => '/ru/275-Kukla-Winx',
+            'Кукла Барби'                                  => '/ru/542-Kukla-Barbi',
+            'Куклы и Пупсы в кор.'                         => '/ru/376-Kukli-i-Pupsi-v-kor',
+            'Куклы и Пупсы пэ'                             => '/ru/377-Kukli-i-Pupsi-pe',
+            'Куклы и Пупсы функциональные'                 => '/ru/357-Kukli-i-Pupsi-funkcionalnie',
+            'Куклы интерактивные'                          => '/ru/1132-Kukli-interaktivnie',
+            'Одежда для кукол и пупсов'                    => '/ru/416-Odezhda-dlya-kukol-i-pupsov',
+            'Посуда, кухни, быт.приборы,овощи-фрукты'      => '/ru/360-Posuda-kuhni-bitpriboriovoschi-frukti',
+            'Пупсы BABY BORN'                              => '/ru/417-Pupsi-BABY-BORN',
+            'Столики кухонные, туалетные'                  => '/ru/363-Stoliki-kuhonnie-tualetnie-doktora-instrumentalnie'),
+
+        'Товары для детей'        => array(
+            'Аксессуары для детей'                                  => '/ru/1094-Aksessuari-dlya-detei',
+            'Докторские наборы'                                     => '/ru/382-Doktora',
+            'Животные'                                              => '/ru/44-Zhivotnie',
+            'Заводные и музыкальные игрушки'                        => '/ru/411-Zavodnie-igrushki-krutie-bobi-muzikalnie',
+            'Игровые центры'                                        => '/ru/1129-Kukla-Defa-Lucy',
+            'Игры Yo-Yo, юла, колечки, калейдоскопы, радуги'        => '/ru/369-Igri-Yo-Yo-yula-kolechki-kaleidoskopi-radugi-svetyashki',
+            'Интерактивные игрушки'                                 => '/ru/368-Govoryaschie-homyaki-i-koti-Tomi',
+            'Карусели на кроватку'                                  => '/ru/354-Karuseli-na-krovatku-mobili',
+            'Каталки'                                               => '/ru/370-Katalki',
+            'Коврики мягкие,музыкальные,для рисования'              => '/ru/355-Kovriki-myagkie-i-muzikalnie',
+            'Копилки, часы'                                         => '/ru/420-Kopilki-chasi',
+            'Молоточки'                                             => '/ru/381-Molotochki',
+            'Музыкальные инструменты'                               => '/ru/366-Muzikalnie-instrumenti',
+            'Мыльные пузыри'                                        => '/ru/532-Milnie-puziri',
+            'Мячики-попрыгунчики резиновые, с наполнителем, лизуны' => '/ru/398-Priguni-rezinovie-s-napolnitelem-lizuni',
+            'Ночники,светильники,проекторы'                         => '/ru/1137-Nochnikisvetilnikiproektori',
+            'Палатки, корзины для игрушек'                          => '/ru/414-Palatki-korzini-dlya-igrushek',
+            'Пищалки, водоплавающие игрушки'                        => '/ru/359-Pischalki-vodoplavayuschie-igrushki',
+            'Погремушки, неваляшки, пирамидки'                      => '/ru/384-Pogremushki-nevalyashki-piramidki',
+            'Радиоуправляемые'                                      => '/ru/1128-Konstruktori-NINJA-TURTLES',
+            'Рыбалки'                                               => '/ru/361-Ribalki',
+            'Светящиеся игрушки'                                    => '/ru/62-Svetyaschiesya-igrushki',
+            'Телефоны, фотоаппараты'                                => '/ru/410-Telefoni-fotoapparati',
+            'Тетрисы, электронные игры'                             => '/ru/421-Tetrisi-elektronnie-igri',
+            'Ходунки, кровати, стулья для кормления'                => '/ru/389-Hodunki-krovati-stulya-dkormleniya'),
+
+        'Товары для мальчиков'    => array(
+            'Бильярд, футбол, хоккей'                   => '/ru/390-Bilyard-futbol-hokkei',
+            'Военные, полиция, пираты, герои в наборах' => '/ru/385-Voennie-policiya-pirati-geroi-v-naborah',
+            'Герои'                                     => '/ru/367-Geroi',
+            'Железные дороги, паркинги, треки'          => '/ru/353-Zheleznie-dorogi-parkingi-treki',
+            'Инерционная техника'                       => '/ru/573-Inercionnaya-tehnika',
+            'Инструменты'                               => '/ru/529-Instrumenti',
+            'Машины и транспорт'                        => '/ru/574-Mashini-i-transport',
+            'Оружие'                                    => '/ru/575-Oruzhie',
+            'Радиоуправляемые модели'                   => '/ru/576-Radioupravlyaemie-modeli',
+            'Трансформеры и роботы'                     => '/ru/388-Transformeri'),
+
+        'Украинский пластик'      => array(
+            'Orion'                         => '/ru/1049-Orion',
+            'Автомобили толокары'           => '/ru/562-Avtomobili-kolyaski',
+            'Горшки'                        => '/ru/923-Gorshki',
+            'Качели'                        => '/ru/925-Kacheli',
+            'Конструкторы'                  => '/ru/553-Konstruktori',
+            'Кубики, шарики, кегли'         => '/ru/555-Kubiki-shariki',
+            'Кухни, посуда,корзины,маркеты' => '/ru/554-Kuhni-posuda',
+            'Логические игры'               => '/ru/557-Logicheskie-igri',
+            'Машинки'                       => '/ru/563-Mashinki',
+            'Музыкальные инструменты'       => '/ru/1026-Muzikalnie-instrumenti',
+            'Настольные игры'               => '/ru/924-Nastolnie-igri',
+            'Песочные наборы'               => '/ru/561-Pesochnie-nabori',
+            'Пирамидки'                     => '/ru/1025-Piramidki',
+            'Сортеры'                       => '/ru/558-Sorteri',
+            'Спортивные'                    => '/ru/560-Sportivnie',
+            'Фламинго'                      => '/ru/955-4Flamingo'),
+
+        'Велосипеды'              => array(
+            '2-х колесные'  => '/ru/979-2-h-kolesnie',
+            '3-х колесные'  => '/ru/977-3-h-kolesnie',
+            'Санки'         => '/ru/1151-Sanki',
+            'Электромобили' => '/ru/1142-Elektromobili'),
+
+        'Спорттовары'             => array(
+            'Аксессуары для спорта'  => '/ru/1074-Aksessuari-dlya-sporta',
+            'Бадминтоны'             => '/ru/734-Badmintoni',
+            'Баскетбол'              => '/ru/796-Basketbol',
+            'Боксерские наборы'      => '/ru/535-Bokserskie-nabori',
+            'Дартс'                  => '/ru/541-Darts',
+            'Защита, шлемы'          => '/ru/249-Zaschita-shlemi',
+            'Мячи'                   => '/ru/544-Myachi',
+            'Мячи резиновые детские' => '/ru/396-Myachi-rezinovie-detskie',
+            'Ролики'                 => '/ru/536-Roliki',
+            'Самокаты'               => '/ru/538-Samokati',
+            'Скакалки'               => '/ru/43-Skakalki',
+            'Скейты'                 => '/ru/344-Skeiti',
+            'Теннис'                 => '/ru/543-Tennis-',
+            'Хулахупы и обручи'      => '/ru/61-Hulahupi-i-obruchi'),
+
+        'Надувные изделия'        => array(
+            'BK Toys Ltd.' => '/ru/1110-BK-Toys-Ltd',
+            'INTEX'        => '/ru/1059-04INTEX'),
+
+        'Канцелярия'              => array(
+            'Офисная канцелярия'      => '/ru/423-Ofisnie-tovari',
+            'Школьные принадлежности' => '/ru/422-Shkolnie-prinadlezhnosti'),
+
+        'Печатная продукция'      => array(
+            'Книги' => '/ru/426-Knigi'),
+
+        'Другие товары для детей' => array(
+            'Мебель для детей' => '/ru/1016-11Mebel-dlya-detei'),
+
+        'Сувенирная продукция'    => array(
+            'Небесные фонарики' => '/ru/1136-Nebesnie-fonariki',
+            'Открытки'          => '/ru/1078-Otkritki',
+            'Подарочные пакеты' => '/ru/1139-Podarochnie-paketi')
+    );
